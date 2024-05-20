692254725c3354c52e425f324f5fd09877da431a
jose R <jose.rodriguez@kashio.net>
2024-05-19 19:01:53 -0500

    update to only chat

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import {
   Component,
   OnInit,
 } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 import { ChatService } from './chat/chat.service';
@@ -10,22 +11,40 @@ import { ChatService } from './chat/chat.service';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HttpClientModule],
+  imports: [RouterOutlet, HttpClientModule, FormsModule],
   providers: [ChatService],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent implements OnInit {
   title = 'chatAngular';
+  message = '';
+  messages: { user: string, text: string }[] = [];
 
 
-  constructor(private ChatService: ChatService) { }
+  constructor(private chatService: ChatService) { }
 
   ngOnInit() {
-    this.ChatService.sendMessage().subscribe((response) => {
+    this.chatService.sendMessage().subscribe((response) => {
       console.log(response);
       debugger;
     });
+  }
+
+
+  sendMessage() {
+    if (this.message.trim()) {
+      this.messages.push({ user: 'You', text: this.message });
+      this.chatService.sendMessage(this.message).subscribe(
+        (response) => {
+          this.messages.push({ user: 'Bot', text: response.response });
+        },
+        (error) => {
+          this.messages.push({ user: 'Error', text: 'Error processing your request' });
+        }
+      );
+      this.message = '';
+    }
   }
 
 }
