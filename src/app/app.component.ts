c5599e891144196d3f8018d8abb1c632409f19d9
jose R <jose.rodriguez@kashio.net>
2024-05-19 20:17:35 -0500

    chatbot fixed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import {
   Component,
@@ -11,7 +12,7 @@ import { ChatService } from './chat/chat.service';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HttpClientModule, FormsModule],
+  imports: [RouterOutlet, HttpClientModule, FormsModule, CommonModule],
   providers: [ChatService],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
@@ -19,32 +20,35 @@ import { ChatService } from './chat/chat.service';
 export class AppComponent implements OnInit {
   title = 'chatAngular';
   message = '';
-  messages: { user: string, text: string }[] = [];
-
+  messages: { recipient_id: string, text: string, isChatbotAnswer: boolean }[] = [];
+  isChatOpen = false;
+  isOpen = false;
 
   constructor(private chatService: ChatService) { }
 
   ngOnInit() {
-    this.chatService.sendMessage().subscribe((response) => {
-      console.log(response);
-      debugger;
-    });
+    /*     this.chatService.sendMessage().subscribe((response) => {
+          console.log(response);
+        }); */
   }
 
-
+  toggleChat() {
+    this.isChatOpen = !this.isChatOpen;
+  }
   sendMessage() {
     if (this.message.trim()) {
-      this.messages.push({ user: 'You', text: this.message });
+      this.messages.push({ recipient_id: '0', text: this.message, isChatbotAnswer: false });
       this.chatService.sendMessage(this.message).subscribe(
         (response) => {
-          this.messages.push({ user: 'Bot', text: response.response });
+          response.forEach((msg: any) => {
+            this.messages.push({ recipient_id: msg.recipient_id, text: msg.text, isChatbotAnswer: true });
+          });
         },
         (error) => {
-          this.messages.push({ user: 'Error', text: 'Error processing your request' });
+          this.messages.push({ recipient_id: 'Error', text: 'Error processing your request', isChatbotAnswer: true });
         }
       );
       this.message = '';
     }
   }
-
 }
